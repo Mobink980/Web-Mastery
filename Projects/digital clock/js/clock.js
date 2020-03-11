ba66e6665a0ef9021d78058e5243d1845e583566
Mobink980 <mobink980@gmail.com>
2020-03-11 11:34:59 +0330

    Update clock.js

--- Projects/digital clock/js/clock.js
+++ Projects/digital clock/js/clock.js
@@ -32,3 +32,41 @@ setInterval(function(){
 
 
 }, 1000);
+
+
+// Determining the day
+var day = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+//some comments!
+var date2 = new Date();
+var weekday2 = new Array(7);
+weekday2[0] = "Begin the Week!";
+weekday2[1] = "Monday morning blues!";
+weekday2[2] = "Taco Time!";
+weekday2[3] = "Two more days to the weekend!";
+weekday2[4] = "The weekend is almost is here...";
+weekday2[5] = "Weekend is here.";
+weekday2[6] = "Time to party!";
+
+
+var n = weekday[day.getDay()];
+var n2 = weekday2[date2.getDay()];
+
+var displayWeekday = document.getElementById('weekday'); //target the strong with id weekday
+var phrase = document.getElementById('phrase'); //target the i with id phrase
+
+
+whatDayIsIt();
+
+function whatDayIsIt(){
+    displayWeekday.innerText = n;
+    phrase.innerText = n2;
+}
